59eed701a3e61c81ccd9c804dff1eb9695664234
Tuan Nguyen <tuan48594@donga.edu.vn>
2021-11-05 22:33:49 +0700

    band

---
+++ assets/JS/script.js/script.js
@@ -0,0 +1,12 @@
+// var money = 100000000;
+// let priceIphone = 32000000;
+function checkSoluong(money, iphone) {
+    if(money < Iphone) {
+        console.log("Bạn k đủ tiền");
+    } else {
+        pay = money / priceIphone;
+        console.log("Số Iphone Bạn có thể mua: " + Math.round(pay))
+    }
+}
+
+checkSoluong(100000000, 32000000);
